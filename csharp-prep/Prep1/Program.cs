c6433790561aa8b0b3e1eb41079d8de287e1cc27
Preciouseze <preciousanietem@gmail.com>
2024-09-21 15:43:31 +0100

    modified:   csharp-prep/Prep1/Program.cs 	modified:   csharp-prep/Prep2/Program.cs 	modified:   csharp-prep/Prep3/Program.cs 	modified:   csharp-prep/Prep4/Program.cs 	modified:   csharp-prep/Prep5/Program.cs

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your First name? ");
+        string First = Console.ReadLine();
+
+        Console.WriteLine("What is your Last name? ");
+        string Last = Console.ReadLine();
+
+        Console.WriteLine ($"Your Firstname is {First}, Your Lastname is {Last}.");
+        Console.WriteLine ($"Your full name is {Last} {First}");
     }
 }
